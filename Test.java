c72f822856ebdff362404d86cf3f29ebc99cc4f6
kuntla123 <kuntlaaish@gmail.com>
2021-10-19 12:41:15 +0530

    updated files by chandni

--- Test.java
+++ Test.java
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+=======
+hiii i am updating this file in uat branch......commit1
+hii i am updating again....commit2
+
+
+
+>>>>>>> 5441047 (commit2)
 Hey...good evening
 
 echo "lets write some linux commands here"
